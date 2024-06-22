6dae33cc675227f73441125979856ff26c375fcf
amar anceur <amaranceur2@gmail.com>
2024-06-23 00:17:24 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+let a = document.getElementById('img');
+let container = document.getElementById('container');// Define the 'container' variable
+
+function phones(phone) {
+    a.src = phone;
+}
+
+function colors(color) {
+    container.style.background = color; // Update the background color of the 'container'
+}
